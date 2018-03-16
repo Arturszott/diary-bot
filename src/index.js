780e1f4b87b6365905015fef10c0c89178426be1
Artur Szott <artur.szott@holidaycheck.com>
2018-03-16 14:49:52 +0100

    Write bot content

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 const http = require('http');
-
 const dotenv = require('dotenv');
-dotenv.load()
+const CachemanFile = require('cacheman-file');
+dotenv.load();
+
+var cache = new CachemanFile({});
 
 // Initialize using verification token from environment variables
 const createSlackEventAdapter = require('@slack/events-api').createSlackEventAdapter;
+const { WebClient } = require('@slack/client');
 const slackEvents = createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN);
 const port = process.env.PORT || 3000;
 
 // Initialize an Express application
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const web = new WebClient(process.env.ACCESS_TOKEN);
 const app = express();
 
 // You must use a body parser for JSON before mounting the adapter
@@ -20,9 +25,61 @@ app.use(bodyParser.json());
 // NOTE: you must mount to a path that matches the Request URL that was configured earlier
 app.use('/slack/events', slackEvents.expressMiddleware());
 
+
+const diaryMessage = `
+Howdy! I see you've entered one of the *#diary* channels. Let me tell you what they are about!
+
+
+During the day team updates what they are doing right now. When everyone are at the office it's easy to know what's going on, but when one of you is working remotely? Maybe EVERYONE works remotely?
+
+
+This is where *#diary* helps.
+
+
+Seeing what other are doing provide you an opportunity to react. You can help them by sharing knowledge on the topic, pair with them on interesting task. It also increases transparency within members which creates a trust amongst members.
+
+
+You were absent for a week? *No problem*. You can *read the most important things* on the channel within minutes.
+ 
+ PSST... please, *comment only via threads* to decrease the noise.
+`;
+
 // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
-slackEvents.on('message', (event)=> {
-  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+slackEvents.on('member_joined_channel', (event)=> {
+  console.log(`User ${event.user} joined channel`);
+
+    cache.get(event.user, function (err, cachedUserID) {
+        if (err) throw err;
+
+        if (cachedUserID) {
+           return;
+        }
+
+        web.channels.list()
+            .then((res) => {
+                const { name } = res.channels.find((channel) => {
+                    return channel.id === event.channel
+                });
+
+                if (!name.endsWith('diary')) {
+                    return;
+                }
+
+                const messageOptions = {
+                    channel: event.user, text: diaryMessage, as_user: true, username: 'Diary Buddy'
+                };
+
+                return web.chat.postMessage(messageOptions)
+            })
+            .then(() => {
+                cache.set(event.user, { visited: Date.now() }, Number.MAX_VALUE, function (err) {
+                    if (err) throw err;
+                });
+
+                console.log('Message sent!');
+            })
+            .catch(console.error);
+    });
 });
 
 // Handle errors (see `errorCodes` export)
